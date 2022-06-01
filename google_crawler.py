4d3f606a2ed30ef45682cf50e75ad5b2312efff6
Minh Thiện <thien.ntm@hahalolo.com>
2022-06-01 16:53:55 +0700

    Update code

--- google_crawler.py
+++ google_crawler.py
@@ -13,21 +13,22 @@ def google_crawler(keyword, num_images):
             google_crawler = GoogleImageCrawler(
                 feeder_threads=1,
                 parser_threads=1,
-                downloader_threads=4,
+                downloader_threads=5,
                 storage={'root_dir': 'download'}
             )
-            filters = dict(
-                size='large',
-                color='orange',
-                license='commercial,modify',
-                date=((2017, 1, 1), (2017, 11, 30)))
+            # filters = dict(
+            #     size='large',
+            #     color='orange',
+            #     license='commercial,modify',
+            #     date=((2017, 1, 1), (2017, 11, 30)))
             # start the crawler with our keyword and number of images
             google_crawler.crawl(
                 keyword=str(keyword)
                 , max_num=int(num_images)
-                , filters=filters
+                # , filters=filters
                 , offset=0, file_idx_offset=0
-                , min_size=(256, 256))
+                # , min_size=(200, 200)
+            )
             # break out of the loop
             break
         except Exception as e:
